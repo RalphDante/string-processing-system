be8066e33fc22d3558ebfa82fb623dc4aa97409c
RalphDante <106255342+RalphDante@users.noreply.github.com>
2026-02-28 11:21:32 +0800

    Add main program to run StringView

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
+using StringProcessingApp.Views;
 
+namespace StringProcessingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringView view = new StringView();
+            view.Run();
+        }
+    }
+}
